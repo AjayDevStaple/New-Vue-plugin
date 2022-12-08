86c195829f91270560d46b3afb5fac2bc8ec07b6
ParamStaple <param.staple@gmail.com>
2022-12-08 12:24:19 +0530

    removed comments

--- src/frontend/components/pages/first_visit_registration.page.vue
+++ src/frontend/components/pages/first_visit_registration.page.vue
@@ -2,167 +2,6 @@
 
     <div class="main mt-3">
 
-        <!-- <h1>First visit registration information</h1>
-
-        <span class="red-subhead mt-3">
-            Remind you!
-        </span>
-        <br />
-        <span class="red-subhead mt-3">
-            1.Only when the information is filled out and archived are the online registration procedures completed.
-        </span>
-        <br />
-
-        <span class="red-subhead">
-            2. Please bring your ID card to the rgistration counter 20 mintues in advance to go through the formalitites
-            before going to the clininc.
-        </span>
-        <br />
-
-        <span>
-            For your health to facilitate doctore's diagnosis and treatement and to save yout consulation time, please
-            be sure to fill in the following indormation correctly!
-        </span>
-
-        <div class="container cont mt-4">
-            <Row class="row">
-                <Col class="col">
-                Name<br />
-
-                <input placeholder="Enter name" v-model="patName" />
-
-                </Col>
-
-                <Col class="row">
-
-                gender
-
-                <input type="radio" id="html" name="male" value="1" v-model="sexType">
-                <label for="html">Male</label>
-
-                <input type="radio" id="html" name="female" value="2" v-model="sexType">
-                <label for="html">Female</label>
-
-
-                </Col>
-            </Row>
-            <hr />
-            <Row class="row">
-                <Col class="col">
-
-                <input type="radio" id="html" name="idNumber" value="1" v-model="idType">
-                  <label for="html">idNumber</label>
-                <input type="radio" id="html" name="passportNumber" value="2" v-model="idType">
-                  <label for="html">Passport No.</label>
-                <input placeholder="Enter Passport No." v-model="patData" />
-                <br />
-
-                <input type="radio" id="html" name="RcardNumber" value="3" v-model="idType">
-                  <label for="html">Residence Card Number</label>
-
-                </Col>
-            </Row>
-            <hr />
-
-            <Row class="row">
-                <Col class="col">
-
-                <span>
-                    cell phone
-                </span>
-                <input placeholder="cell phone number" v-model="mobile" />
-
-                </Col>
-            </Row>
-
-            <hr />
-            <Row class="row">
-                <Col class="col">
-
-                <span>Date of Birth (Date of the Republic of China)</span>
-                <input placeholder="date of birth" v-model="birthDate" />
-                </Col>
-            </Row>
-
-            <hr />
-
-            <Row class="row">
-
-
-
-                <Col class="col">
-
-                <span>
-                    Residence address
-                    <select id="cars" name="cars" defaultValue="1" v-model="foreverCity">
-                        <option value="1">Please Check</option>
-                        <option value="2">Volvo</option>
-                        <option value="3">Saab</option>
-                        <option value="4">Fiat</option>
-                        <option value="5">Audi</option>
-                    </select> <input placeholder="Postal code" v-model="foreverZipCode" /> <input
-                        placeholder="Please enter the complete address" v-model="foreverAddrName" />
-
-
-                </span>
-
-                </Col>
-                <Col class="col">
-
-                <select id="cars" name="cars" defaultValue="1" v-model="foreverAddrNameFlag">
-                    <option value="1">Please Check</option>
-                    <option value="2">Volvo</option>
-                    <option value="3">Saab</option>
-                    <option value="4">Fiat</option>
-                    <option value="5">Audi</option>
-                </select>
-
-
-
-
-                </Col>
-
-
-            </Row>
-            <hr />
-
-            <Row class="row">
-
-                <Col class="col">
-                <span>
-                    <input type="checkbox" id="vehicle1" name="vehicle1" @change="check($event)"
-                        v-model="sameAddress" />
-                    same registerd address
-                </span>
-
-                <br />
-                <span>
-                    Mailing Address <span> <select id="cars" name="cars" defaultValue="1" v-model="tmpCity">
-                            <option value="1">Please Check</option>
-                            <option value="2">Volvo</option>
-                            <option value="3">Saab</option>
-                            <option value="4">Fiat</option>
-                            <option value="5">Audi</option>
-                        </select> <input placeholder="Postal code" v-model="tmpZipCode" /> <input
-                            placeholder="Please enter the complete address" v-model="tmpAddrName" /> </span> 
-                </span>
-
-
-                </Col>
-                <Col class="col">
-
-                    <select id="cars" name="cars" defaultValue="1" v-model="tmpAddrNameFlag">
-                        <option value="1">Please Check</option>
-                        <option value="2">Volvo</option>
-                        <option value="3">Saab</option>
-                        <option value="4">Fiat</option>
-                        <option value="5">Audi</option>
-                    </select>
-                    </Col>
-            </Row>
-
-            <hr /> -->
-
         <div class="custom-registration-form">
 
             <h2 class="mjTitle">First Visit Registration Form</h2>
@@ -338,6 +177,11 @@
 
 
         </div>
+
+
+       
+
+
 
 
 
